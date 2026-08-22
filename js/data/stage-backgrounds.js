71510307ca32d95920a41a1156ab7744bf1be7b9
Mohammad <moeltaher@users.noreply.github.com>
2026-08-23 00:12:31 +0300

    Register explicit training recovery scene

--- js/data/stage-backgrounds.js
+++ js/data/stage-backgrounds.js
@@ -4,11 +4,13 @@ export const SCENES_BY_STAGE = {
   datacenter: ['ch3Intro','dcInstall','dcCooling','dcCoolingOutcome','dcWorkers','abstract3'],
   data: ['ch4Intro','dataOrigins','dataClean','dataFollowup','dataCleanSummary','abstract4'],
   annotation: ['ch5Intro','annotationIntro','annotationTask','annotationReview','annotationEnd','abstract5'],
-  training: ['ch6Intro','trainingSetup','trainingRun','trainingEval','abstract6'],
+  training: ['ch6Intro','trainingSetup','trainingRun','trainingRecovery','trainingEval','abstract6'],
   evaluation: ['ch7Intro','evalTask','checkpointEval','safetyTest','safetyOutcome','safetyRetest','launchDecision','launchOutcome','abstract7'],
   deployment: ['ch8Intro','deployLoad','deployIncident','onCall','supportTask','deployEnd','abstract8'],
   ending: ['intro','zoomOut','pipelineAssemble','transferChallenge','finalAnswer','results','finalMessage']
 };
+
+export const ALL_SCENES = Object.values(SCENES_BY_STAGE).flat();
 
 const STAGE_BACKDROPS = {
   mining: './assets/images/scenes/01-mining.svg',
